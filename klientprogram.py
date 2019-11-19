8058b25f5067e1729323e85d1e558f6877c07746
Emil <emil.jonsson2@yh.nackademin.se>
2019-11-19 16:55:28 +0100

    versin 1.1.1

--- klientprogram.py
+++ klientprogram.py
@@ -2,57 +2,92 @@ from appJar import gui
 import socket
 from threading import Thread
 
-
-def showGui():
-    app = gui("ShatApp", "500x500")
-    app.setBg("lightslategrey")
-    app.addLabel(
-        "welcomemsg", "Write a aliasname below and press connect to start using ShatApp!")
-
-    app.startFrame("first")
-    app.setBg("lightslategrey")
-    app.addEntry("namn", row=0)
-    app.addButtons(["connect", "show users", "quit"], [
-                   btncallback, btncallback, btncallback])
-    app.stopFrame()
-    app.startFrame("second")
-    app.setBg("lightslategrey")
-    app.setSticky("wsn")
-    app.addVerticalSeparator(colour="black", row=0, column=0)
-    app.setSticky("ew")
-    app.addListBox("chat", row=0, column=1)
-    app.setSticky("esn")
-    app.addVerticalSeparator(colour="black", row=0, column=2, colspan=4)
-    app.stopFrame()
-    app.startFrame("third")
-    app.setBg("lightslategrey")
-    app.addEntry("write")
-    app.addButtons(["send", "ABC", "abc"], [
-                   btncallback, btncallback, btncallback])
-    app.stopFrame()
-    app.go()
-
-
-def btncallback(btn):
-    if btn == "connect":
-        pass
-    if btn == "quit":
-        pass
-
+public_chat_list= []
+list_of_users= ["all"]
 
 def connect_to_server():
-    pass
+    HOST_name = socket.gethostname()
+    HOST_ip = socket.gethostbyname(HOST_name)
+    PORT = 65432
+    global mySocket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mySocket:
+        mySocket.connect((HOST_ip, PORT))
+        print("connected to server and thread for listening established")
+        
+        while True:
+            listen_thread = Thread(target=recieve, args=(mySocket,))
+            listen_thread.start()
+            
+            
+def send(message, mySocket):
+
+    b = bytes(message, 'utf-8')
+    mySocket.sendall(b)
 
 
-def recieve():
-    pass
 
 
-def main():
-    pass
+def recieve(mySocket):
+    while True:
+        data = mySocket.recv(1024).decode('utf-8')
+        if not data:
+            break
+        app.updateListBox("chat",public_chat_list)
 
 
-if __name__ == "__main__":
-    main()
+    
 
-showGui()
+def btncallback(btn):
+
+    if btn == "connect":
+        alias = app.getEntry("namn")
+        list_of_users.append(alias)
+        app.changeOptionBox("user", list_of_users)
+        app.thread(connect_to_server)
+    if btn == "quit":
+        send("close", mySocket)
+        mySocket.close()
+    if btn == "send":
+        message = app.getEntry("write")
+        current_option = app.getOptionBox("user")
+        if current_option !="all":
+            print("it works")
+        public_chat_list.append(message)
+        send(message, mySocket)
+        app.clearEntry("write")
+        
+        
+
+
+app = gui("ShatApp", "500x500")
+app.setBg("lightslategrey")
+app.addLabel(
+    "welcomemsg", "Write a aliasname below and press connect to start using ShatApp!")
+app.startFrame("first")
+app.setBg("lightslategrey")
+app.setStretch("both")
+app.addEntry("namn", row=0,column=1,colspan=1)
+app.stopFrame()
+app.startFrame("second")
+app.setPadding("10","10")
+app.setSticky("ew")
+app.addButtons(["connect", "quit"], [btncallback, btncallback],row=1,column=0)
+app.addOptionBox("user", list_of_users,row=1,column=1)
+app.stopFrame()
+app.startFrame("third")
+app.setBg("lightslategrey")
+app.setSticky("wsn")
+app.addVerticalSeparator(colour="black", row=0, column=0)
+app.setSticky("ew")
+app.addListBox("chat", row=0, column=1)
+app.setSticky("esn")
+app.addVerticalSeparator(colour="black", row=0, column=2, colspan=4)
+app.stopFrame()
+app.startFrame("fourth")
+app.setBg("lightslategrey")
+app.addEntry("write")
+app.addButtons(["send", "ABC", "abc"], [
+    btncallback, btncallback, btncallback])
+app.stopFrame()
+
+app.go()
--- serverprogram.py
+++ serverprogram.py
@@ -11,6 +11,7 @@ def server_gui():
 
 def listen_bind(HOST, PORT):
     mySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     mySock.bind((HOST, PORT))
     mySock.listen()
     print('listening...')
@@ -18,20 +19,29 @@ def listen_bind(HOST, PORT):
         conn, adress = mySock.accept()
         print(conn, adress)
         dict_of_users[conn] = adress
-        Thread(target=recive_from_user, args=(conn,)).start()
-    return
+        recive_thread = Thread(target=recive_from_user, args=(conn,))
+        recive_thread.start()
+
+    return recive_thread
 
 
-def send_all():
-    pass
+def send_all(data):
+    for key in dict_of_users:
+        key.sendall(data)
 
 
 def send_private():
     pass
 
 
-def recive_from_user():
-    pass
+def recive_from_user(conn):
+    while True:
+        data = conn.recv(1024).decode('utf-8')
+        if not data:
+            break
+        data = format(data)
+        returnvalue = data.encode('utf-8')
+        send_all(returnvalue)
 
 
 def main():
@@ -41,7 +51,9 @@ def main():
     print("Hostname :  ", HOST_name)
     print("IP : ", HOST_ip)
 
-    listen_bind(HOST_ip, PORT)
+    recive_thread = listen_bind(HOST_ip, PORT)
+    print(recive_thread.is_alive())
+    recive_thread.join()
 
 
 if __name__ == "__main__":
